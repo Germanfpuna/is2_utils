9b0452ac3221da073ae5d7ca4ada481ded730ca1
Germanfpuna <ger13gm28@fpuna.edu.py>
2024-11-11 01:25:47 -0300

    Refactor production settings to use environment variables for database configuration

--- configs/settings_produccion.py
+++ configs/settings_produccion.py
@@ -1,6 +1,5 @@
 from .settings import *
 
-
 """
 ADVERTENCIA DE SEGURIDAD: No ejecutes con depuración activada en producción.
 """
@@ -12,13 +11,12 @@ Configuración de la base de datos.
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cms',
-        'USER': 'postgres',
-        'PASSWORD': '99583854',
-        'HOST': 'db',
-        'PORT': '5432',
+        'NAME': config('DEPLOY_DATABASE_NAME'),
+        'USER': config('DEPLOY_DATABASE_USER'),
+        'PASSWORD': config('DEPLOY_DATABASE_PASSWORD'),
+        'HOST': config('DEPLOY_DATABASE_HOST'),
+        'PORT': config('DEPLOY_DATABASE_PORT'),
         'CHARSET': 'UTF8',
     }
 }
-
 
